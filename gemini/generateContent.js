c463fe66acf7e595f984da6644887ca5edcaa9db
SR Tamim <saifur.rahman.tamim1@gmail.com>
2024-04-13 19:55:27 +0600

    Add generateContent.js for content generation

---
+++ gemini/generateContent.js
@@ -0,0 +1,10 @@
+
+const ai = require("../gemini/geminiAI")
+
+async function getContentResponse(prompt) {
+    const res = await ai.generateContent(prompt)
+    const txt = res.response.text()
+    return txt
+}
+
+module.exports = { getContentResponse }
